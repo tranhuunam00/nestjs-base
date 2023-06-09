e77d405a61f1e00f0e3bae23e24acdf5a847338f
namth <tranhuunam23022000@gmail.com>
2023-06-09 23:24:49 +0700

    sersor modal

---
+++ src/models/sensor.model.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument, ObjectId } from 'mongoose'
+import { ModelsBase } from './interface/base.model.interface'
+
+export type SensorDocument = HydratedDocument<Sensor>
+
+@Schema()
+export class SensorProp extends ModelsBase {
+  @Prop({ type: String, required: true })
+  username: string
+
+  @Prop({ type: String, required: true })
+  id_user: string
+
+  @Prop({ type: Date, required: true })
+  time: Date
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  value: string
+}
+
+export class Sensor extends SensorProp {
+  @Prop({ type: String })
+  _id: ObjectId
+}
+
+export const SensorSchema = SchemaFactory.createForClass(SensorProp).set(
+  'timestamps',
+  true
+)
